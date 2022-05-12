cdee311e217f7c0dc55abe1f737c61666bb02b79
Jason Williams <jasonw@jhu.edu>
2022-05-12 17:04:35 -0400

    update extra_packages script

--- image-gen/extra_packages.sh
+++ image-gen/extra_packages.sh
@@ -4,7 +4,7 @@
 # Author: Jason Williams <jasonw@.jhu.edu>
 
 
-pkgs=`cat /tmp/mprov/entity.json | jq -r .config_params | yq --unwrapScalar '.extra_packages'`
+pkgs=`cat /tmp/mprov/entity.json | jq -r .config_params | yq --unwrapScalar  .extra_packages  | sed -e 's/\s*\-\s*//'
 
 
 if [ "$pkgs" == "" ]
