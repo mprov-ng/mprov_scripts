92a7be6a4cc18350b957b3e509d4f8822adc5f28
Jason Williams <jasonw@jhu.edu>
2022-05-12 16:42:08 -0400

    adding extra packages script.

---
+++ image-gen/extra_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# extra_packages.sh - install a list of extra packages from config-params
+# Author: Jason Williams <jasonw@.jhu.edu>
+
+
+pkgs=`cat /tmp/mprov/entity.json | jq -r .config_params | yq --unwrapScalar '.extra_packages'`
+
+
+if [ "$pkgs" == "" ]
+then
+  echo "No extra packages."
+  exit 0
+fi
+
+dnf -y install $pkgs
