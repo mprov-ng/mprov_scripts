301e808695f55307260280d2e1a9c64349221cf9
Jason Williams <jasonw@jhu.edu>
2022-09-13 13:34:11 -0400

    rename script-runner script.

--- image-gen/install_mprov_script-runner.sh
+++ image-gen/install_mprov_script-runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script will install the mprov_jobserver to run postboot.
+# This script will install the mprov_jobserver to run script-runner.
 # It will make some files in /etc/mprov/ for the jobserver and
 # the script-runner plugins.  script-runner should handle
 # all the rest of the setup for the postboot scripts after
@@ -10,18 +10,28 @@ cd /tmp/mprov
 # get some initial variables.
 mprovURL=`cat entity.json | jq .mprovURL -r`
 apikey=`cat entity.json | jq .apikey -r`
+entityName=`cat entity.json | jq .slug -r`
+echo "Entity Name: $entityName"
+if [ "$entityName" == "nads" ]
+then
+    mod="nads"
+    descr="The mProv Node Auto Detection System (NADS)"
+else
+    mod="script-runner"
+    descr="The mProv Jobserver for postboot scripts"
+fi
 
 mkdir -p /etc/mprov/plugins
 
 # the jobserver config
-cat << EOF > /etc/mprov/jobserver.yaml
+cat << EOF > /etc/mprov/${mod}.yaml
 - global:
     mprovURL: "$mprovURL"
     apikey: '$apikey'
     heartbeatInterval: 10
     runonce: True # uncomment to run the jobserver once and exit.
     jobmodules:
-      - script-runner
+      - ${mod}
 - !include plugins/*.yaml
 EOF
 
@@ -31,22 +41,27 @@ cat << EOF > /etc/mprov/plugins/script-runner.yaml
     scriptTmpDir: '/tmp/mprov'
 EOF
 
+# NADS config
+cat << EOF > /etc/mprov/plugins/nads.yaml
+- nads:
+    reboot: True
+EOF
+
 # the systemd service file
-cat << EOF > /usr/lib/systemd/system/mprov_jobserver_postboot.service
+cat << EOF > /usr/lib/systemd/system/mprov-${mod}.service
 [Unit]
-Description=The mProv Jobserver for postboot scripts
+Description=$descr 
 After=network-online.target
 Wants=network-online.target
 
 [Service]
 Type=oneshot
-# runonce, in system mode, in post boot mode, use the /etc/mprov/jobserver.yaml
-ExecStart=/usr/local/bin/mprov_jobserver -d -r -s -b -c /etc/mprov/jobserver.yaml
+# runonce, in system mode, in post boot mode, use the /etc/mprov/${mod}.yaml
+ExecStart=/usr/local/bin/mprov_jobserver -d -r -s -b -c /etc/mprov/${mod}.yaml
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
 # enable the systemd service
-systemctl enable mprov_jobserver_postboot
-
+systemctl enable mprov-${mod}
