6dddd8549abaaf40914bbc5c61b014cd73f20a98
Jason Williams <jasonw@jhu.edu>
2023-06-13 17:02:01 -0400

    fix up typo in mprov share script.

--- image-gen/setup_mprov_share_mount.sh
+++ image-gen/setup_mprov_share_mount.sh
@@ -7,7 +7,7 @@ dnf -y install nfs-utils
 
 mkdir -p /opt/mprov
 # Add bind mount to fstab
-grep -qF '/export/mprov' /etc/fstab || echo '${mpccHost}:/export/mprov    /opt/mprov    nfs defaults    0 0' >> /etc/fstab
+grep -qF '/export/mprov' /etc/fstab || echo "${mpccHost}:/export/mprov    /opt/mprov    nfs defaults    0 0" >> /etc/fstab
 
 cat << EOF > /etc/profile.d/99-mprov.sh
 export PATH=/opt/mprov/bin:$PATH
