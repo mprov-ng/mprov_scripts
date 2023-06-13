7614d80423ac232fb299074af90f507e716e8716
Jason Williams <jasonw@jhu.edu>
2023-06-13 12:12:08 -0400

    script to setup the mProv Network Share

---
+++ image-gen/setup_mprov_share_mount.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mprovURL=`cat entity.json | jq .mprovURL -r`
+mpccHost=`echo "$mprovURL" | awk -F/ '{print $3}'`
+
+dnf -y install nfs-utils
+
+mkdir -p /opt/mprov
+# Add bind mount to fstab
+grep -qF '/export/mprov' /etc/fstab || echo '${mpccHost}:/export/mprov    /opt/mprov    nfs defaults    0 0' >> /etc/fstab
+
+cat << EOF > /etc/profile.d/99-mprov.sh
+export PATH=/opt/mprov/bin:$PATH
+
+if [ "$USER" == "root" ]
+then
+  export PATH=/opt/sbin/:$PATH
+fi
+EOF
