03fde3246789b12260f42ccd949419f1fb75f320
Jason Williams <jasonw@jhu.edu>
2023-09-12 11:41:07 -0400

    first attempt at an import script

---
+++ tools/import_systems_csv.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# This script will import a CSV of nodes into mProv.  
+# Currently, the following fields are required:
+#
+#   nodename,mac,ipaddress
+# 
+
+help {
+  echo "Usage: $0 <file> [-i iface_name] [-m image_name]" >&2
+  exit 1
+}
+
+mashBin=`which mash`
+if [ ! -x "$mashBin" ]
+then
+  echo "Error: mash Binary not found/not executable."
+  exit 1
+fi
+
+if [ "$1" == "" ]
+then
+  help
+fi
+
+if [ ! -e "$1" ]
+then
+  echo "Error: File $1 not accessible."
+  exit 1
+fi
+
+filename=$1
+shift
+
+interfaceName=eno1
+imageName=compute
+
+while [[ $# -gt 0 ]]
+do
+  case $1 in 
+    -i)
+      shift
+      interfaceName=$1
+      shift
+      ;;
+    -m)
+      shift
+      imageName=$1
+      shift
+      ;;
+    *)
+      echo "Error: Unknown arg $1"
+      exit 1
+      ;;
+  esac
+done
+
+echo -n "" > /tmp/mash_node_import
+
+for entry in `cat $filename`
+do
+  IFS=',' read -ra DATA <<< "$entry"
+  m_hostname=${DATA[0]}
+  m_macaddr=${DATA[1]}
+  m_ipaddr=${DATA[2]}
+  cat << EOF >> /tmp/mash_node_import
+
+print Creating System ${m_hostname}
+create systems.models.System hostname=${m_hostname} created_by=1 tmpfs_root_size=0 stateful=0 systemimage=${imageName}
+let nodeId = {{ MPROV_RESULT['id'] }}
+
+# add the nic
+create systems.models.NetworkInterface name=ens0 hostname=${m_hostname} ipaddress=${{m_ipaddr}} bootable=1 system={{nodeId}}
+
+
+EOF
+
+done
+
+# Engage...
+echo "connect" | cat << /tmp/mash_node_import | mash
