3673b8145123d463aea8abd7ca2c2b1d7011faf0
Jason Williams <jasonw@jhu.edu>
2023-10-31 10:15:43 -0400

    add eof to script

--- post-boot/add-cuda-to-path.sh
+++ post-boot/add-cuda-to-path.sh
@@ -4,3 +4,4 @@ rm -f /etc/profile.d/99-cuda.sh
 export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64
 export PATH=\$PATH:\$CUDA_HOME/bin
+EOF
